b3d024cb79a6c076606b93679976f7be0a4ec547
lghtlr <sunny_grapes@mail.ru>
2024-11-05 23:31:58 +0300

    empty project

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+// import { AppController } from './0_app.controller';
+// import { AppService } from './0_app.service';
+
+@Module({
+  imports: [],
+  controllers: [], //AppController],
+  providers: [], //AppService],
+})
+export class AppModule {}
