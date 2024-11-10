eaeffdca6235d7138728b44db22001444e882c20
lghtlr <sunny_grapes@mail.ru>
2024-11-10 21:09:35 +0300

    changed main files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CoursesModule } from './courses/courses.module';
 import { UsersModule } from './users/users.module';
 import { OrdersModule } from './orders/orders.module';
+import { DatasourceModule } from './datasource /datasource.module';
 
 @Module({
-  imports: [CoursesModule, UsersModule, OrdersModule],
+  imports: [CoursesModule, UsersModule, OrdersModule, DatasourceModule],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = new DocumentBuilder()
+    .setTitle('Courses Site API')
+    .setVersion('1.0')
+    .build() // сконфигурировала сборщик документации
+
+    const document = SwaggerModule.createDocument(app, config); // создала api документацию
+
+    SwaggerModule.setup('api_docs', app, document) // включила доку Swagger по пути localhost:3001/api_docs
+
+    await app.listen(3001); // установила порт прослушивания 3001
+
+    app.setGlobalPrefix('/api'); // установила глобальный префикс для роутов контроллеров
 }
 bootstrap();
