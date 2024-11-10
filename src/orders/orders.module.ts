c32bde4b2d6ad48834d4d814c3c4ea9c0730b1f1
lghtlr <sunny_grapes@mail.ru>
2024-11-10 21:32:38 +0300

    fix import

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common'
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './order.service';
-import { DataSource } from 'typeorm';
+import { DatasourceModule } from 'src/datasource/datasource.module';
 import { Order } from './order.entity';
 
 @Module({
     controllers: [OrdersController],
     providers: [OrdersService],
-    imports: [Order, DataSource],
+    imports: [Order, DatasourceModule],
 })
 export class OrdersModule { }
