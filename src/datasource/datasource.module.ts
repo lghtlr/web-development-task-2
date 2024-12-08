a60bfe2483b31073dc45ccaf2e5a5a05f433e8ed
lghtlr <sunny_grapes@mail.ru>
2024-12-08 16:13:27 +0300

    Comment Datasource

--- src/datasource/datasource.module.ts
+++ src/datasource/datasource.module.ts
@@ -1,8 +1,8 @@
-import { Module } from "@nestjs/common";
-import { DatasourceService } from "./datasource.service";
+// import { Module } from "@nestjs/common";
+// import { DatasourceService } from "./datasource.service";
 
-@Module({
-    providers: [DatasourceService], // "тут мы указали, что у нас есть сервис внутри модуля"
-    exports: [DatasourceService], // "тут мы разрешили экспортировать сервис в другие модули"
-})
-export class DatasourceModule { }
+// @Module({
+//     providers: [DatasourceService], // "тут мы указали, что у нас есть сервис внутри модуля"
+//     exports: [DatasourceService], // "тут мы разрешили экспортировать сервис в другие модули"
+// })
+// export class DatasourceModule { }
